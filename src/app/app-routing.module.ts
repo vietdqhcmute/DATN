3db4907d0d9031478bec34ac6904543aabc11d54
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-03-15 22:28:32 +0700

    Module login ok, adding boostrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
+  { path:"all-job", loadChildren: "./layout/layout.module#LayoutModule"},
   { path: "login", loadChildren: "./login/login.module#LoginModule" }
 ];
 
