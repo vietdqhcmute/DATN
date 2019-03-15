10c2be7ac8640d3549c65dbbbdc4f608c9118224
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-03-15 23:17:17 +0700

    Mess up with module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,64 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import {
+  MatSelectModule,
+  MatSlideToggleModule,
+  MatAutocompleteModule,
+  MatProgressBarModule,
+  MatDialogModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule,
+  MatListModule,
+  MatSliderModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatMenuModule,
+  MatSnackBarModule,
+  MatTabsModule,
+  MatProgressSpinnerModule
+} from "@angular/material";
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatAutocompleteModule,
+    MatProgressBarModule,
+    MatDialogModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatSliderModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatMenuModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatProgressSpinnerModule
+  ],
+  exports: [
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatAutocompleteModule,
+    MatProgressBarModule,
+    MatDialogModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatSliderModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatMenuModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatProgressSpinnerModule
+  ]
+})
+export class MaterialModule {}
