f260c0271f9c3d955088384f1ea0615dc4bf1768
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-03-15 22:31:58 +0700

    Create sign up recruiter and candidate

---
+++ src/app/register/components/register-candidate/register-candidate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-candidate',
+  templateUrl: './register-candidate.component.html',
+  styleUrls: ['./register-candidate.component.scss']
+})
+export class RegisterCandidateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegisterCandidateComponent } from './components/register-candidate/register-candidate.component';
+import { RegisterRecruiterComponent } from './components/register-recruiter/register-recruiter.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [RegisterCandidateComponent, RegisterRecruiterComponent],
   imports: [
     CommonModule
   ]
