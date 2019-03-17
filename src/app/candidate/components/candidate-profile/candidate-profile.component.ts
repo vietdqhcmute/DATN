c7c5da73d365390d19a8088bd12e895cfe7fb2c2
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-03-17 22:59:55 +0700

    Add profile page

--- src/app/candidate/components/candidate-profile/candidate-profile.component.ts
+++ src/app/candidate/components/candidate-profile/candidate-profile.component.ts
@@ -1,21 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { Candidate } from 'src/app/models/CandidateData';
-
+import { Component, OnInit } from "@angular/core";
+import { Candidate } from "src/app/models/CandidateData";
+import { CandidateService } from "src/app/services/candidate.service";
+import { first } from "rxjs/operators";
 @Component({
-  selector: 'app-candidate-profile',
-  templateUrl: './candidate-profile.component.html',
-  styleUrls: ['./candidate-profile.component.scss']
+  selector: "app-candidate-profile",
+  templateUrl: "./candidate-profile.component.html",
+  styleUrls: ["./candidate-profile.component.scss"]
 })
 export class CandidateProfileComponent implements OnInit {
   isAuthenticated = false;
   email: string;
-  candidate: Candidate=null;
+  candidate: Candidate = null;
   testEmail = "vietdqhcmute@gmail.com";
   allowEdit = false;
-  defaultImageURL = "../../../../assets/images/tho-bay-mau-28.png"
-  constructor() { }
+  defaultImageURL = "../../../../assets/images/tho-bay-mau-28.png";
+  constructor(private candidateService: CandidateService) {}
 
   ngOnInit() {
+    this.loadCandidateData(this.testEmail);
   }
-
+  private onEditButton() {
+    if (this.allowEdit) {
+      this.candidateService
+        .updateCandidateByID(this.candidate._id, this.candidate)
+        .subscribe(
+          response => {
+            console.log("Done!");
+          },
+          error => {
+            console.log("Error!");
+          }
+        );
+    }
+    this.allowEdit = !this.allowEdit;
+  }
+  private loadCandidateData(email) {
+    this.candidateService
+      .getCandidateByEmail(email)
+      .pipe(first())
+      .subscribe(candidate => {
+        this.candidate = <Candidate>candidate;
+      });
+  }
 }
