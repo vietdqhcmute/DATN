c429c021cba528f1e9a6aa853147a49cd5cbf151
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-03-17 15:45:44 +0700

    Increase login UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from './material/material.module';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     MaterialModule,
     HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/candidate/candidate.module.ts
+++ src/app/candidate/candidate.module.ts
@@ -4,13 +4,15 @@ import { CandidateComponent } from './candidate.component';
 import { CandidateProfileComponent } from './components/candidate-profile/candidate-profile.component';
 import { CandidateRoutingModule } from './candidate-routing.module';
 import { PartialModule } from '../partial/partial.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [CandidateComponent, CandidateProfileComponent],
   imports: [
     CommonModule,
     CandidateRoutingModule,
-    PartialModule
+    PartialModule,
+    RouterModule
   ]
 })
 export class CandidateModule { }
