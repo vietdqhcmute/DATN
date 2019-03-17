5c52330b7b1a1a175c01438b201eaf809ca6cd70
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-03-17 10:50:45 +0700

    Fix can not click routerLink

--- src/app/partial/partial.module.ts
+++ src/app/partial/partial.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { CompanyFrameComponent } from './company-frame/company-frame.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [TopBarComponent, CompanyFrameComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[CompanyFrameComponent,TopBarComponent]
 })
