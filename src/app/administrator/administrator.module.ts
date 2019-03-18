d18812c77f53cd1fa3bb95e4103ec05458af1e89
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-03-18 23:17:25 +0700

    Create admin page

--- src/app/administrator/administrator.module.ts
+++ src/app/administrator/administrator.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdministratorComponent } from './administrator.component';
 import { AdministratorRoutingModule } from './administrator-routing.module';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
-  declarations: [AdministratorComponent],
+  declarations: [AdministratorComponent, SidebarComponent, HeaderComponent],
   imports: [
     CommonModule,
     AdministratorRoutingModule
