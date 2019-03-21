4f5f6c5dc7827fd574e6a4efe97cbc741e05c06e
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-03-21 19:26:21 +0700

    Create Job and company description

---
+++ src/app/layout/company-description/company-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-description',
+  templateUrl: './company-description.component.html',
+  styleUrls: ['./company-description.component.scss']
+})
+export class CompanyDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,9 +4,11 @@ import { LayoutComponent } from './layout.component';
 import { AllJobComponent } from './all-job/all-job.component';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { PartialModule } from '../partial/partial.module';
+import { JobDescriptionComponent } from './job-description/job-description.component';
+import { CompanyDescriptionComponent } from './company-description/company-description.component';
 
 @NgModule({
-  declarations: [LayoutComponent, AllJobComponent],
+  declarations: [LayoutComponent, AllJobComponent, JobDescriptionComponent, CompanyDescriptionComponent],
   imports: [
     CommonModule,
     LayoutRoutingModule,
