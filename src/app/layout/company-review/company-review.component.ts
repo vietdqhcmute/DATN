97263434a3dfed49b91171d379b84fcb3df38ab5
Vinh <nguyenthevinh1997@gmail.com>
2019-03-27 23:30:36 +0700

    Create rating star finish

--- src/app/layout/company-review/company-review.component.ts
+++ src/app/layout/company-review/company-review.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from "@angular/core";
 
+interface ICompany {
+  id: number;
+  rating: string;
+  evaluation: string;
+}
 @Component({
   selector: "app-company-review",
   templateUrl: "./company-review.component.html",
   styleUrls: ["./company-review.component.scss"]
 })
 export class CompanyReviewComponent implements OnInit {
-  isDivVisible = true;
+  isShowCriticism = true;
+  isShowShareCompany = true;
+  ratingClicked: number;
+  itemIdRatingClicked: string;
+  items: ICompany[] = [
+    { id: 0, rating: '', evaluation: "Đánh giá tổng quát" },
+    { id: 1, rating: '', evaluation: "Lương thưởng và phúc lợi" },
+    { id: 2, rating: '', evaluation: "Đào tạo và học hỏi " },
+    { id: 3, rating: '', evaluation: "Sự quan tâm đến nhân viên" },
+    { id: 4, rating: '', evaluation: "Văn hóa công ty" },
+    { id: 5, rating: '', evaluation: "Văn phòng làm việc" }
+  ];
   constructor() {}
-  show() {
-    this.isDivVisible = this.isDivVisible? false : true;
+  showCriticism() {
+    this.isShowCriticism = this.isShowCriticism ? false : true;
+  }
+  showShareCompany() {
+    this.isShowShareCompany = this.isShowShareCompany ? false : true;
+  }
+  ratingComponentClick(clickObj: any): void {
+    console.log(clickObj);
+    const item = this.items.find((i: any) => i.id === clickObj.itemId);
+    if (!!item) {
+      item.rating = clickObj.rating;
+      this.ratingClicked = clickObj.rating;
+      this.itemIdRatingClicked = item.evaluation;
+    }
   }
   ngOnInit() {}
 }
--- src/app/partial/rating-star/rating-star.component.ts
+++ src/app/partial/rating-star/rating-star.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-rating-star',
-  templateUrl: './rating-star.component.html',
-  styleUrls: ['./rating-star.component.scss']
+  selector: "app-rating-star",
+  templateUrl: "./rating-star.component.html",
+  styleUrls: ["./rating-star.component.scss"]
 })
 export class RatingStarComponent implements OnInit {
-
-  constructor() { }
+  @Input() rating: string;
+  @Input() itemId: number;
+  @Output() ratingClick: EventEmitter<any> = new EventEmitter<any>();
+  inputName: string;
+  constructor() {}
 
   ngOnInit() {
+    this.inputName = this.itemId + "_rating";
   }
-
+  onClick(rating: string): void {
+    this.rating = rating;
+    console.log(this.rating);
+    this.ratingClick.emit({
+      itemId: this.itemId,
+      rating: rating
+    });
+  }
 }
