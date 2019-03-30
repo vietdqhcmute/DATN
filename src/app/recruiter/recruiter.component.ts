c12f8b4e5c44f6063592ecdbf3f56beea38eb212
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-03-30 14:46:49 +0700

    Change to use email instead company-name and save post

--- src/app/recruiter/recruiter.component.ts
+++ src/app/recruiter/recruiter.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { AuthService } from "../services/auth.service";
 import { RecruiterService } from "../services/recruiter.service";
 import { Recruiter } from "../models/RecruiterData";
 import { ActivatedRoute } from "@angular/router";
@@ -14,25 +13,27 @@ import { Title } from '@angular/platform-browser';
 export class RecruiterComponent implements OnInit {
   image_url =
     "https://cdn.itviec.com/employers/amanotes/logo/w170/8dM6PZybgr1ahE2Fr2pac4bm/amanotes-logo.png";
-  paramsCompanyEmail: String;
   recruiterData: Recruiter;
+
   constructor(
-    private authService: AuthService,
-    private recruiterService: RecruiterService,
-    private route: ActivatedRoute,
+    protected recruiterService: RecruiterService,
+    protected route: ActivatedRoute,
     private titleService: Title
   ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
-      this.paramsCompanyEmail = params.get("email");
-      this.loadRecruiterData(this.paramsCompanyEmail);
-      this.titleService.setTitle("Profile of "+ this.paramsCompanyEmail);
+      this.loadRecruiterData(params.get("email"));
+      this.titleService.setTitle("Profile of "+ params.get("email"));
     });
   }
-  onLogOut() {
+
+
+
+  private onLogOut() {
     // this.authService.logOut();
   }
+
   private loadRecruiterData(email) {
     this.recruiterService
       .getRecruiterByEmail(email)
