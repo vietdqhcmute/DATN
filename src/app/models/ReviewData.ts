70d5ee6b1589f4e64a9eea3d43242c5bf5373924
viet.do <viet.do@saleshood.com>
2019-03-31 22:54:32 +0700

    Create review model

---
+++ src/app/models/ReviewData.ts
@@ -0,0 +1,15 @@
+export class ReviewPost {
+  company_email: String;
+  rate_general: Number;
+  rate_salary: Number;
+  rate_training: Number;
+  rate_care: Number;
+  rate_culture: Number;
+  rate_infrastructure: Number;
+  title: String;
+  ot_like: String;
+  ot_hate: String;
+  like: String;
+  hate: String;
+  introduce: Boolean;
+}
