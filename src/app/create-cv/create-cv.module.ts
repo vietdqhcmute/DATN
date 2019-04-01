64b44453d0a6f057e2547df572a60373ee3d8ab3
viet.do <viet.do@saleshood.com>
2019-04-01 23:23:48 +0700

    Add new modal dialog in profile page

--- src/app/create-cv/create-cv.module.ts
+++ src/app/create-cv/create-cv.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateCvRoutingModule } from './create-cv-routing.module';
+import { PartialModule } from '../partial/partial.module';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CreateCvRoutingModule
+    CreateCvRoutingModule,
+    PartialModule,
+    RouterModule,
+    FormsModule,
+    MaterialModule
   ]
 })
 export class CreateCvModule { }
