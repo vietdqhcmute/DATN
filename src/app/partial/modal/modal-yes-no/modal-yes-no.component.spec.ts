956f3ed54b52e7b05a3ee7ea822cb96f1974a5bf
Vinh <nguyenthevinh1997@gmail.com>
2019-05-02 15:53:03 +0700

    Create modal yes no

---
+++ src/app/partial/modal/modal-yes-no/modal-yes-no.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModalYesNoComponent } from './modal-yes-no.component';
+
+describe('ModalYesNoComponent', () => {
+  let component: ModalYesNoComponent;
+  let fixture: ComponentFixture<ModalYesNoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ModalYesNoComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ModalYesNoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
