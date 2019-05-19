2627b5c8b525879aea7e8af7168f0a8649061401
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-05-19 18:03:44 +0700

    Refactor all job page

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from "@angular/core";
 import { RecruiterService } from "./recruiter.service";
+import { Articles } from '../models/RecruiterData';
 
 @Injectable({
   providedIn: "root"
 })
 export class ArticleService extends RecruiterService {
+  searchArticle(searchText: string) {
+    return this.http.post<Articles[]>(
+      this.domainName + "search/articles?" + "key=" + searchText,
+      {}
+    );
+  }
   getAllArticles(email) {
     return this.http.get<any>(this.domainName + "articles/" + email);
   }
@@ -14,13 +21,14 @@ export class ArticleService extends RecruiterService {
   }
 
   saveArticle(requestBody: any, email: string) {
-    this.http
-      .post(this.domainName + "article/", requestBody)
-      .subscribe(response => {
-        this.router.navigate(["recruiter", email, "dashboard"])
-      }, error=>{
+    this.http.post(this.domainName + "article/", requestBody).subscribe(
+      response => {
+        this.router.navigate(["recruiter", email, "dashboard"]);
+      },
+      error => {
         this.alertService.error(error);
-      });
+      }
+    );
   }
 
   updateArticle(requestBody: any) {}
