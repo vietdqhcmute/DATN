54eedf0b94ecc701b2b3e5cb1b289fc3da337237
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-05-21 18:37:48 +0700

    Temp done search on UI

--- src/app/layout/search-article/search-article.component.ts
+++ src/app/layout/search-article/search-article.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ArticleService } from "src/app/services/article.service";
+import { Articles } from "src/app/models/RecruiterData";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-search-article',
-  templateUrl: './search-article.component.html',
-  styleUrls: ['./search-article.component.scss']
+  selector: "app-search-article",
+  templateUrl: "./search-article.component.html",
+  styleUrls: ["./search-article.component.scss"]
 })
 export class SearchArticleComponent implements OnInit {
-
-  constructor() { }
+  private articles: Articles[] = [];
+  private searchText: string;
+  constructor(
+    protected route: ActivatedRoute,
+    private router: Router,
+    private ariticlesService: ArticleService
+  ) {}
 
   ngOnInit() {
+    this.articles = [];
+    this.getQueryParams();
+    this.searchArticles(this.searchText);
   }
 
+  getQueryParams() {
+    this.route.queryParams.subscribe(query => {
+      this.searchText = query.key;
+    });
+  }
+
+  searchArticles(searchText: string) {
+    this.ariticlesService.searchArticle(searchText).subscribe(articles => {
+      this.articles = [];
+      this.articles = articles;
+    });
+  }
+
+  onSearch() {
+    this.router.navigate(["search"], { queryParams: { key: this.searchText } });
+    this.getQueryParams();
+    this.searchArticles(this.searchText);
+  }
 }
