7271d5c0d8b44c0ac1c726c71e51435c636580ce
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-05-27 23:24:53 +0700

    Create tag data backend and service

--- server/routes/tag-routes.js
+++ server/routes/tag-routes.js
@@ -3,30 +3,54 @@ const router = express.Router();
 const Tag = require("../models/Tag");
 
 //Adding tag
-router.post("/add/tag", (req, res) => {
+router.post("/tag", (req, res) => {
   //Check if tag has already exist
-  Tag.findOne({tag_name: req.body.tag_name},(err,data)=>{
-    if (!data){
+  Tag.findOne({ content: req.body.content }, (err, data) => {
+    if (!data) {
       const tag = new Tag(req.body);
-      tag.save((err,data) => {
+      tag.save((err, data) => {
         if (err) {
           res.status(500);
-          return console.log(err);      
+          return console.log(err);
         }
-        res.status(200).send(data);
+        return res.status(200).send(data);
       });
+    }else{
+      return res.status(401).send("Tag duplicate");
     }
-    return res.status(500);
-  })
+  });
 });
 //Getting by tag by name
-router.get("/tag/:tag_name", async (req, res) => {
+router.get("/tag/:content", async (req, res) => {
   try {
-    const tag = await Tag.findOne({tag_name: req.params.tag_name});
+    const tag = await Tag.findOne({ content: req.params.content });
     res.status(200).json(tag);
   } catch (err) {
     res.status(404).send(err);
   }
+});
+//Getting all tags
+router.get("/tags", async (req, res) => {
+  try {
+    const tag = await Tag.find();
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+//Delete tag
+router.delete("/tag", (req, res) => {
+  Tag.findOneAndRemove({ content: req.body.content }, (err, callback) => {
+    if (err) {
+      return res.status(500).send(err);
+    } else {
+      if(!callback){
+        return res.status(404).send("Doesn't exist!");
+      }
+      return res.status(200).json("Delete successfully!");
+    }
+  });
 });
 
 module.exports = router;
