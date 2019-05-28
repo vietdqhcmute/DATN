ad56f67236a6d7dd2a41db783a01830cb3f555ec
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-05-28 16:59:39 +0700

    Modal tag front end

--- src/app/partial/modal/modal-tag/modal-tag.component.ts
+++ src/app/partial/modal/modal-tag/modal-tag.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-modal-tag',
-  templateUrl: './modal-tag.component.html',
-  styleUrls: ['./modal-tag.component.scss']
+  selector: "app-modal-tag",
+  templateUrl: "./modal-tag.component.html",
+  styleUrls: ["./modal-tag.component.scss"]
 })
 export class ModalTagComponent implements OnInit {
+  private searchTagText: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
