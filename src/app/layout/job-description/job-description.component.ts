77eda97bfbdaf3b942b7ad369a0ca2ab8b7655d2
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-06-01 15:45:58 +0700

    Upgrade job description

--- src/app/layout/job-description/job-description.component.ts
+++ src/app/layout/job-description/job-description.component.ts
@@ -10,7 +10,7 @@ import { ArticleService } from 'src/app/services/article.service';
   styleUrls: ["./job-description.component.scss"]
 })
 export class JobDescriptionComponent implements OnInit {
-  private companyInfo = {
+  private recruiterInfo = {
     _id: "",
     image_url: "",
     company_name: "",
@@ -18,9 +18,10 @@ export class JobDescriptionComponent implements OnInit {
     type: "",
     employees: "",
     date_at_work: "",
-    email:""
+    email:"",
+    slogan:""
   };
-  private jobInfo = {
+  private articleInfo = {
     _id: "",
     title: "",
     salary: "",
@@ -45,24 +46,25 @@ export class JobDescriptionComponent implements OnInit {
     this.sub = this.articleService
       .getArticleById(id)
       .subscribe(jobData => {
-        this.jobInfo._id = jobData._id;
-        this.jobInfo.title = jobData.title;
-        this.jobInfo.salary = jobData.salary;
-        this.jobInfo.created_at = jobData.created_at;
-        this.jobInfo.description = jobData.description;
+        this.articleInfo._id = jobData._id;
+        this.articleInfo.title = jobData.title;
+        this.articleInfo.salary = jobData.salary;
+        this.articleInfo.created_at = jobData.created_at;
+        this.articleInfo.description = jobData.description;
       });
   }
   private getCompanyData(email) {
     this.sub = this.recruiterService
       .getRecruiterAPI(email)
       .subscribe(response => {
-        this.companyInfo._id = response._id;
-        this.companyInfo.image_url = response.image_url;
-        this.companyInfo.company_name = response.company_name;
-        this.companyInfo.addr = response.addr;
-        this.companyInfo.employees = response.employees;
-        this.companyInfo.date_at_work = response.date_at_work;
-        this.companyInfo.email = response.email;
+        this.recruiterInfo._id = response._id;
+        this.recruiterInfo.image_url = response.image_url;
+        this.recruiterInfo.company_name = response.company_name;
+        this.recruiterInfo.addr = response.addr;
+        this.recruiterInfo.employees = response.employees;
+        this.recruiterInfo.date_at_work = response.date_at_work;
+        this.recruiterInfo.email = response.email;
+        this.recruiterInfo.slogan = response.slogan;
       });
   }
 }
