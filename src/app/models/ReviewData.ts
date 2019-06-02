98c7e0cf6c2cf3c41492da3d2e269b4a250dd1e1
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-06-02 10:54:16 +0700

    Changing review

--- src/app/models/ReviewData.ts
+++ src/app/models/ReviewData.ts
@@ -1,12 +1,12 @@
 export class Review {
   email: String;
+  title: String;
   rate_general: Number;
   rate_salary: Number;
   rate_training: Number;
   rate_care: Number;
   rate_culture: Number;
   rate_infrastructure: Number;
-  title: String;
   ot_like: String;
   ot_hate: String;
   like: String;
@@ -15,13 +15,13 @@ export class Review {
   created_at: Date;
   constructor() {
     this.email = "";
+    this.title = "";
     this.rate_general = 0;
     this.rate_salary = 0;
     this.rate_training = 0;
     this.rate_care = 0;
     this.rate_culture = 0;
     this.rate_infrastructure = 0;
-    this.title = "";
     this.ot_like = "";
     this.ot_hate = "";
     this.like = "";
