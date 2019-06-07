ceabd5b6d7397fe0b2588e8cac591982e43f84c8
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-06-08 00:00:28 +0700

    Refactor code

--- src/app/layout/company-description/company-description.component.ts
+++ src/app/layout/company-description/company-description.component.ts
@@ -11,27 +11,28 @@ export class CompanyDescriptionComponent extends RecruiterComponent
   implements OnInit, OnDestroy {
   imageURL_company =
     "https://cdn.itviec.com/photos/35827/processed_headline_photo/fpt-software-headline_photo.png?ojMRBGzhWEL7ri4CE7w3VgwU";
-  sub: Subscription;
   company_email: String;
   reviews = [];
   ngOnInit() {
-    this.sub = this.route.paramMap.subscribe(params => {
-      this.company_email = params.get("email");
-      this.loadRecruiterData(params.get("email"));
-      this.getReviews(params.get("email"));
-    });
+    this.sub.push(
+      this.route.paramMap.subscribe(params => {
+        this.company_email = params.get("email");
+        this.loadRecruiterData(params.get("email"));
+        this.getReviews(params.get("email"));
+      })
+    );
   }
-
-  getReviews(email){
-    this.reviewService.getAllReviewByEmail(email).subscribe(data => {
-      this.reviews = data;
-      console.log(this.reviews);
-    });
+  ngOnDestroy(): void {
+    this.sub.forEach(subscription => subscription.unsubscribe());
   }
   onReview() {
     this.router.navigate(["review", this.company_email]);
   }
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
+  getReviews(email) {
+    this.sub.push(
+      this.reviewService.getAllReviewByEmail(email).subscribe(data => {
+        this.reviews = data;
+      })
+    );
   }
 }
--- src/app/layout/search-company/search-company.component.ts
+++ src/app/layout/search-company/search-company.component.ts
@@ -1,33 +1,42 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { RecruiterService } from "src/app/services/recruiter.service";
 import { Recruiter } from "src/app/models/RecruiterData";
 import { NgForm } from "@angular/forms";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-search-company",
   templateUrl: "./search-company.component.html",
   styleUrls: ["./search-company.component.scss"]
 })
-export class SearchCompanyComponent implements OnInit {
-  private searchText: string;
-  private recruiters: Recruiter[] = [];
+export class SearchCompanyComponent implements OnInit, OnDestroy {
+  searchText: string;
+  recruiters: Recruiter[] = [];
+  sub: Subscription[] = [];
+
   constructor(private recruiterService: RecruiterService) {}
   ngOnInit() {
     this.getAllCompany();
   }
-
+  ngOnDestroy(): void {
+    this.sub.forEach(subscription => subscription.unsubscribe());
+  }
   onSearch(form: NgForm) {
     this.searchCompany(this.searchText);
   }
 
   searchCompany(searchText: string) {
-    this.recruiterService.searchCompany(searchText).subscribe(companies => {
-      this.recruiters = companies;
-    });
+    this.sub.push(
+      this.recruiterService.searchCompany(searchText).subscribe(companies => {
+        this.recruiters = companies;
+      })
+    );
   }
   getAllCompany() {
-    this.recruiterService.getAllRecruites().subscribe(companies => {
-      this.recruiters = companies;
-    });
+    this.sub.push(
+      this.recruiterService.getAllRecruites().subscribe(companies => {
+        this.recruiters = companies;
+      })
+    );
   }
 }
