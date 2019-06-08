31d34b29f207148565631831d6792edf383ead35
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-06-08 18:24:32 +0700

    subscription destroy

--- src/app/partial/material-dialog/dialog-applier-list/dialog-applier-list.component.ts
+++ src/app/partial/material-dialog/dialog-applier-list/dialog-applier-list.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit, Inject } from "@angular/core";
+import { Component, OnInit, Inject, OnDestroy } from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
 import { Router } from "@angular/router";
 import { ArticleService } from "src/app/services/article.service";
 import { CandidateService } from "src/app/services/candidate.service";
 import { Candidate } from "src/app/models/CandidateData";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-dialog-applier-list",
   templateUrl: "./dialog-applier-list.component.html",
   styleUrls: ["./dialog-applier-list.component.scss"]
 })
-export class DialogApplierListComponent implements OnInit {
+export class DialogApplierListComponent implements OnInit, OnDestroy {
   _id: string;
   appliers: [string];
   private candidates: Candidate[] = [];
+  sub: Subscription[] = [];
   constructor(
     private router: Router,
     private articleService: ArticleService,
@@ -30,24 +32,37 @@ export class DialogApplierListComponent implements OnInit {
       this.onApplyList();
     }
   }
-
+  ngOnDestroy(): void {
+    this.unsubscribeData();
+  }
   close() {
+    this.unsubscribeData();
     this.dialogRef.close();
   }
   onApplyList() {
-    this.articleService
-      .getAllAppliersByArticleId(this._id)
-      .subscribe(appliers => {
-        appliers.forEach(email => {
-          this.candidateService.getCandidateAPI(email).subscribe(candidate => {
-            this.candidates.push(<Candidate>candidate);
+    this.sub.push(
+      this.articleService
+        .getAllAppliersByArticleId(this._id)
+        .subscribe(appliers => {
+          appliers.forEach(email => {
+            this.candidateService
+              .getCandidateAPI(email)
+              .subscribe(candidate => {
+                this.candidates.push(<Candidate>candidate);
+              });
           });
-        });
-      });
+        })
+    );
   }
   onViewApplierCV(email: string) {
     this.router.navigate(["profile", email, "create-cv"], {
       queryParams: { edit: false }
+    });
+  }
+
+  unsubscribeData() {
+    this.sub.forEach(subscription => {
+      subscription.unsubscribe();
     });
   }
 }
