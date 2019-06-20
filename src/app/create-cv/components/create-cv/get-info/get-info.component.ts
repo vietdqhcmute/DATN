4f366edc73505575bd965dd92e3fe718ce6ffafc
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-06-20 16:54:12 +0700

    Create UI for mat dialog resume

--- src/app/create-cv/components/create-cv/get-info/get-info.component.ts
+++ src/app/create-cv/components/create-cv/get-info/get-info.component.ts
@@ -7,6 +7,10 @@ import {
   Project,
   Education
 } from "src/app/models/CandidateData";
+import { MatDialogConfig } from "@angular/material";
+import { DialogResumeExperienceComponent } from "src/app/partial/material-dialog/dialog-resume-experience/dialog-resume-experience.component";
+import { DialogResumeEducationComponent } from "src/app/partial/material-dialog/dialog-resume-education/dialog-resume-education.component";
+import { DialogResumeProjectComponent } from "src/app/partial/material-dialog/dialog-resume-project/dialog-resume-project.component";
 
 @Component({
   selector: "app-get-info",
@@ -17,7 +21,28 @@ export class GetInfoComponent extends CreateCvComponent implements OnInit {
   @Input() getInfoResume: Resume;
   @Input() getInfoCandidate: Candidate;
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onAddExperience() {
+    const dialogConfig = this.configDefaultMatDialog();
+    const dialogRef = this.dialog.open(
+      DialogResumeExperienceComponent,
+      dialogConfig
+    );
+  }
+  onAddEducation() {
+    const dialogConfig = this.configDefaultMatDialog();
+    const dialogRef = this.dialog.open(
+      DialogResumeEducationComponent,
+      dialogConfig
+    );
+  }
+  onAddProject() {
+    const dialogConfig = this.configDefaultMatDialog();
+    const dialogRef = this.dialog.open(
+      DialogResumeProjectComponent,
+      dialogConfig
+    );
   }
 
   onSaveExperience(modalExperience: Experience) {
@@ -31,12 +56,20 @@ export class GetInfoComponent extends CreateCvComponent implements OnInit {
   }
 
   onDeleteExperience(index: number) {
-    this.getInfoResume.experience.splice(index,1);
+    this.getInfoResume.experience.splice(index, 1);
   }
   onDeleteProject(index: number) {
-    this.getInfoResume.project.splice(index,1);
+    this.getInfoResume.project.splice(index, 1);
   }
   onDeleteEducation(index: number) {
-    this.getInfoResume.education.splice(index,1);
+    this.getInfoResume.education.splice(index, 1);
+  }
+
+  configDefaultMatDialog(): MatDialogConfig {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+
+    return dialogConfig;
   }
 }
--- src/app/partial/material-dialog/dialog-resume-experience/dialog-resume-experience.component.ts
+++ src/app/partial/material-dialog/dialog-resume-experience/dialog-resume-experience.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { Experience } from "src/app/models/CandidateData";
 
 @Component({
-  selector: 'app-dialog-resume-experience',
-  templateUrl: './dialog-resume-experience.component.html',
-  styleUrls: ['./dialog-resume-experience.component.scss']
+  selector: "app-dialog-resume-experience",
+  templateUrl: "./dialog-resume-experience.component.html",
+  styleUrls: ["./dialog-resume-experience.component.scss"]
 })
 export class DialogResumeExperienceComponent implements OnInit {
+  constructor(
+    private dialogRef: MatDialogRef<DialogResumeExperienceComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Experience
+  ) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
