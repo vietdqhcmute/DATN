6cc3f5e8f8e99c286e79f29662f4d5a3b7c5c8ed
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-06-29 14:34:37 +0700

    Push article and candidate in report document by tag

--- server/routes/report-routes.js
+++ server/routes/report-routes.js
@@ -28,5 +28,40 @@ router.get("/tags", async (req, res) => {
   console.log(reportTag);
   return res.status(200).send(reportTag);
 });
-
+// Push user in candidate field
+router.put("/tag/candidate", async (req, res) => {
+  let reportTag = await ReportTag.findOne({ _tag: req.body._tag });
+  if (reportTag._candidates.indexOf(req.body.candidate_id) === -1) {
+    //If candidate not in this tag before
+    reportTag._candidates.push(req.body.candidate_id);
+    reportTag._candidates.sort();
+    reportTag.candidates_count = reportTag._candidates.length;
+    reportTag.save();
+    console.log(reportTag);
+    return res.status(200).send("Success!");
+  } else {
+    console.log(reportTag._candidates);
+    return res
+      .status(401)
+      .send("Has tagged before");
+  }
+});
+//Push article in article field
+router.put("/tag/article", async (req, res) => {
+  let reportTag = await ReportTag.findOne({ _tag: req.body._tag });
+  if (reportTag._articles.indexOf(req.body.article_id) === -1) {
+    //If candidate not in this tag before
+    reportTag._articles.push(req.body.article_id);
+    reportTag._articles.sort();
+    reportTag.articles_count = reportTag._articles.length;
+    reportTag.save();
+    console.log(reportTag);
+    return res.status(200).send("Success!");
+  } else {
+    console.log(reportTag._articles);
+    return res
+      .status(401)
+      .send("Has tagged before");
+  }
+});
 module.exports = router;
