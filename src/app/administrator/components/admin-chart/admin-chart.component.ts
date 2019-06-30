a91975ea6bc91875c7dc0889b8a0062f2f2b9b64
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-06-30 13:39:39 +0700

    Add chart

--- src/app/administrator/components/admin-chart/admin-chart.component.ts
+++ src/app/administrator/components/admin-chart/admin-chart.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { Chart } from "chart.js";
 
 @Component({
-  selector: 'app-admin-chart',
-  templateUrl: './admin-chart.component.html',
-  styleUrls: ['./admin-chart.component.scss']
+  selector: "app-admin-chart",
+  templateUrl: "./admin-chart.component.html",
+  styleUrls: ["./admin-chart.component.scss"]
 })
 export class AdminChartComponent implements OnInit {
+  @ViewChild("canvas") canvas: ElementRef;
+  title = "Dash board";
+  chart: any = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  chartInit() {
+    const data = {};
+    const options = {};
+    this.chart = new Chart(this.canvas.nativeElement.getContext("2d"), {
+      type: "bar",
+      data: data,
+      options: options
+    });
   }
-
 }
