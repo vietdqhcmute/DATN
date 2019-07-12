b6b6fb0603f0951dbd0efd6fea321aac60932809
Do Quoc Viet <vietdqhcmute@gmail.com>
2019-07-12 12:03:45 +0700

    change to server host

--- server/server.js
+++ server/server.js
@@ -5,9 +5,9 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 
-// const connectionString = "mongodb://lytutronga6:lytutronga6@datn-cluster-shard-00-00-p9e6i.mongodb.net:27017,datn-cluster-shard-00-01-p9e6i.mongodb.net:27017,datn-cluster-shard-00-02-p9e6i.mongodb.net:27017/test?ssl=true&replicaSet=DATN-cluster-shard-0&authSource=admin&retryWrites=true"
+const connectionString = "mongodb://lytutronga6:lytutronga6@datn-cluster-shard-00-00-p9e6i.mongodb.net:27017,datn-cluster-shard-00-01-p9e6i.mongodb.net:27017,datn-cluster-shard-00-02-p9e6i.mongodb.net:27017/test?ssl=true&replicaSet=DATN-cluster-shard-0&authSource=admin&retryWrites=true"
 // connection to db
-const connectionString = "mongodb://localhost:27017/datn"; //local mongodb
+// const connectionString = "mongodb://localhost:27017/datn"; //local mongodb
 mongoose.disconnect().then(
   mongoose
     .connect(connectionString, {
